54c3c7158ee7401e7a42563bf1b92c1a75b394dd
Walter Garcia <walter.garciaortiz@gmail.com>
2023-02-12 21:45:12 +0000

    Add files via upload
    first version, reverse cipher

---
+++ reverse_cipher.py
@@ -0,0 +1,11 @@
+# Reverse cipher
+
+message = "Three can keep a secret, if two of them are dead"
+translated = ""
+
+i = len(message) - 1
+while i >= 0:
+    translated = translated + message[i]
+    i = i - 1
+
+print(translated)
